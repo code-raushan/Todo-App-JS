d8931caf718a599e88b5315563a8a60449918ed6
code-raushan <raushan.development@gmail.com>
2022-10-26 11:45:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const container = document.querySelector('.container');
+const addBtn = document.querySelector('.addButton');
+
+function createTodo(){
+    let div = document.createElement('div');
+    div.classList.add('item');
+    container.appendChild(div);
+
+    let innerDiv = document.createElement('div');
+    div.appendChild(innerDiv);
+
+    innerDiv.classList.add('item_input');
+    innerDiv.innerHTML = document.querySelector('.input').value;
+
+    let btnContainer = document.createElement('div');
+    btnContainer.setAttribute('class', 'btn-container');
+    btnContainer.style.marginTop = '10px'
+    div.appendChild(btnContainer);
+
+    let editBtn = document.createElement('button');
+    editBtn.innerText = 'edit';
+    editBtn.classList.add('editButton');
+    btnContainer.appendChild(editBtn);
+
+    editBtn.addEventListener('click', ()=>{    
+        innerDiv.contentEditable = true;
+        editBtn.innerText = 'done'
+        editBtn.addEventListener('click', ()=>{        
+            editBtn.innerText = 'edit';
+            innerDiv.contentEditable = false;
+        })
+
+    })
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'delete';
+    deleteBtn.classList.add('deleteButton');
+    btnContainer.appendChild(deleteBtn);  
+    
+    deleteBtn.addEventListener('click', ()=>{
+        container.removeChild(div);
+    })
+
+    
+}
+function todo(){
+    //creating the todo block    
+    createTodo();
+    document.querySelector('.input').value = '';   
+}
+addBtn.addEventListener('click', todo);
+
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# A Basic Todo App built purely in JavaScript
+
+A basic Todo App implementation in Vanilla JavaScript with UI made with HTML and CSS.
+
+Best project for performing CRUD operations in JS without the help of any frameworks.
+
+
+
+Here is how the project looks:
+![todo](./todo-pic.png)
